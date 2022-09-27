8b539efe26f34fce05dd6c05a5cf48701866c19a
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-09-27 22:13:07 +0530

    new

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,62 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+   private var isFinishedTypingNumber:Bool = true
+    private var displayValue:Double{
+        get{
+            guard let number = Double(displayLabel.text!) else{
+                fatalError("Cannot covert text to a double")
+            }
+            return number
+        }
+        set{
+            displayLabel.text = String(newValue)
+        }
+        
+    }
+    
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+       
+        if let calMethod = sender.currentTitle{
+            if calMethod == "+/-"{
+                displayValue = displayValue * -1
+            }
+            else if calMethod == "AC"{
+                displayLabel.text = ""
+            }
+           else if calMethod == "%"{
+               displayValue = displayValue/100
+            }
+        }
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle{
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }else{
+                if numValue == "."{
+                    }
+                    let isint = floor(displayValue) == displayValue
+                    if isint{
+                        return
+                    }
+                }
+                
+                
+                
+                displayLabel.text = displayLabel.text!+numValue
+            }
+        }
     
     }
 
-}
+
 
